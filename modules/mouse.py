5d11c2a1ef376d148d1ebd39dd9e6fefaafe3da7
Masahiquo, Naraya MORRY <kerberos.gm@gmail.com>
2025-05-31 09:37:53 +0900

    little fix, add mouse center button with shift?

--- modules/mouse.py
+++ modules/mouse.py
@@ -11,17 +11,24 @@ from modules.variables import mod4
 
 
 mouse = [
+  # Left button
   Drag([mod4], 'Button1',
     lazy.window.set_position_floating(), start = lazy.window.get_position(),
     # desc = 'Move in Mouse',
   ),
-  Drag([mod4], 'Button3',
-    lazy.window.set_size_floating(), start = lazy.window.get_size(),
-    # desc = 'Resize in Mouse',
-  ),
+  # Center button
   Click([mod4], 'Button2',
     lazy.window.bring_to_front(),
     # desc = 'Front in Mouse',
+  ),
+  Click([mod4, 'shift'], 'Button2',
+    lazy.window.move_down(),
+    # desc = 'Bottom in Mouse',
+  ),
+  # Right button
+  Drag([mod4], 'Button3',
+    lazy.window.set_size_floating(), start = lazy.window.get_size(),
+    # desc = 'Resize in Mouse',
   ),
 ]
 
